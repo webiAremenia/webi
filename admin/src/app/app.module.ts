ecf44e8204b6f48e299b49f2fac8a159d6d72fa1
SaqoNode <sargisyeritsyan1@gmail.com>
2019-04-10 11:41:03 +0400

    admin commit

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './components/pages/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import {InterceptorService} from './_services/interceptor.service';
 import {DashboardComponent} from './components/pages/dashboard/dashboard.component';
 import {SideNavComponent} from './components/partials/side-nav/side-nav.component';
@@ -14,46 +14,53 @@ import {PortfolioCreateComponent} from './components/pages/portfolio/portfolio-c
 import {PortfolioEditComponent} from './components/pages/portfolio/portfolio-edit/portfolio-edit.component';
 import {PortfolioViewComponent} from './components/pages/portfolio/portfolio-view/portfolio-view.component';
 import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
-import { HeaderComponent } from './components/partials/header/header.component';
-import { MediaListComponent } from './components/pages/media/media-list/media-list.component';
-import { MediaCreateComponent } from './components/pages/media/media-create/media-create.component';
-import { MediaViewComponent } from './components/pages/media/media-view/media-view.component';
-import { MediaEditComponent } from './components/pages/media/media-edit/media-edit.component';
-import { SettingListComponent } from './components/pages/setting/setting-list/setting-list.component';
-import { SettingViewComponent } from './components/pages/setting/setting-view/setting-view.component';
-import { SettingEditComponent } from './components/pages/setting/setting-edit/setting-edit.component';
-import { SettingCreateComponent } from './components/pages/setting/setting-create/setting-create.component';
-import { PageListComponent } from './components/pages/page/page-list/page-list.component';
-import { PageViewComponent } from './components/pages/page/page-view/page-view.component';
-import { PageEditComponent } from './components/pages/page/page-edit/page-edit.component';
-import { PageCreateComponent } from './components/pages/page/page-create/page-create.component';
-import { TeamListComponent } from './components/pages/team/team-list/team-list.component';
-import { TeamViewComponent } from './components/pages/team/team-view/team-view.component';
-import { TeamCreateComponent } from './components/pages/team/team-create/team-create.component';
-import { TeamEditComponent } from './components/pages/team/team-edit/team-edit.component';
+import {HeaderComponent} from './components/partials/header/header.component';
+import {MediaListComponent} from './components/pages/media/media-list/media-list.component';
+import {MediaCreateComponent} from './components/pages/media/media-create/media-create.component';
+import {MediaViewComponent} from './components/pages/media/media-view/media-view.component';
+import {MediaEditComponent} from './components/pages/media/media-edit/media-edit.component';
+import {SettingListComponent} from './components/pages/setting/setting-list/setting-list.component';
+import {SettingViewComponent} from './components/pages/setting/setting-view/setting-view.component';
+import {SettingEditComponent} from './components/pages/setting/setting-edit/setting-edit.component';
+import {SettingCreateComponent} from './components/pages/setting/setting-create/setting-create.component';
+import {PageListComponent} from './components/pages/page/page-list/page-list.component';
+import {PageViewComponent} from './components/pages/page/page-view/page-view.component';
+import {PageEditComponent} from './components/pages/page/page-edit/page-edit.component';
+import {PageCreateComponent} from './components/pages/page/page-create/page-create.component';
+import {TeamListComponent} from './components/pages/team/team-list/team-list.component';
+import {TeamViewComponent} from './components/pages/team/team-view/team-view.component';
+import {TeamCreateComponent} from './components/pages/team/team-create/team-create.component';
+import {TeamEditComponent} from './components/pages/team/team-edit/team-edit.component';
 import {SortablejsModule} from 'angular-sortablejs';
-import { TitleDescriptionFilterPipe } from './_pipes/title-description-filter.pipe';
-import { FullNameFilterPipe } from './_pipes/full-name-filter.pipe';
-import { KeyFilterPipe } from './_pipes/key-filter.pipe';
-import { CategoryListComponent } from './components/pages/category/category-list/category-list.component';
-import { CategoryViewComponent } from './components/pages/category/category-view/category-view.component';
-import { CategoryEditComponent } from './components/pages/category/category-edit/category-edit.component';
-import { CategoryCreateComponent } from './components/pages/category/category-create/category-create.component';
-import { NameFilterPipe } from './_pipes/name-filter.pipe';
-import { MenuListComponent } from './components/pages/menu/menu-list/menu-list.component';
-import { MenuCreateComponent } from './components/pages/menu/menu-create/menu-create.component';
-import { MenuEditComponent } from './components/pages/menu/menu-edit/menu-edit.component';
-import { MenuViewComponent } from './components/pages/menu/menu-view/menu-view.component';
-import { MenuSortComponent } from './components/pages/menu/menu-sort/menu-sort.component';
+import {TitleDescriptionFilterPipe} from './_pipes/title-description-filter.pipe';
+import {FullNameFilterPipe} from './_pipes/full-name-filter.pipe';
+import {KeyFilterPipe} from './_pipes/key-filter.pipe';
+import {CategoryListComponent} from './components/pages/category/category-list/category-list.component';
+import {CategoryViewComponent} from './components/pages/category/category-view/category-view.component';
+import {CategoryEditComponent} from './components/pages/category/category-edit/category-edit.component';
+import {CategoryCreateComponent} from './components/pages/category/category-create/category-create.component';
+import {NameFilterPipe} from './_pipes/name-filter.pipe';
+import {MenuListComponent} from './components/pages/menu/menu-list/menu-list.component';
+import {MenuCreateComponent} from './components/pages/menu/menu-create/menu-create.component';
+import {MenuEditComponent} from './components/pages/menu/menu-edit/menu-edit.component';
+import {MenuViewComponent} from './components/pages/menu/menu-view/menu-view.component';
+import {MenuSortComponent} from './components/pages/menu/menu-sort/menu-sort.component';
 import {NgxTreeDndModule} from "ngx-tree-dnd";
-import { LanguageListComponent } from './components/pages/language/language-list/language-list.component';
-import { LanguageCreateComponent } from './components/pages/language/language-create/language-create.component';
-import { LanguageViewComponent } from './components/pages/language/language-view/language-view.component';
-import { LanguageEditComponent } from './components/pages/language/language-edit/language-edit.component';
-import { NewsListComponent } from './components/pages/news/news-list/news-list.component';
-import { NewsCreateComponent } from './components/pages/news/news-create/news-create.component';
-import { NewsEditComponent } from './components/pages/news/news-edit/news-edit.component';
-import { NewsViewComponent } from './components/pages/news/news-view/news-view.component';
+import {LanguageListComponent} from './components/pages/language/language-list/language-list.component';
+import {LanguageCreateComponent} from './components/pages/language/language-create/language-create.component';
+import {LanguageViewComponent} from './components/pages/language/language-view/language-view.component';
+import {LanguageEditComponent} from './components/pages/language/language-edit/language-edit.component';
+import {NewsListComponent} from './components/pages/news/news-list/news-list.component';
+import {NewsCreateComponent} from './components/pages/news/news-create/news-create.component';
+import {NewsEditComponent} from './components/pages/news/news-edit/news-edit.component';
+import {NewsViewComponent} from './components/pages/news/news-view/news-view.component';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {NgSelectModule} from "@ng-select/ng-select";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -113,8 +120,16 @@ import { NewsViewComponent } from './components/pages/news/news-view/news-view.c
     ReactiveFormsModule,
     HttpClientModule,
     CKEditorModule,
-    SortablejsModule.forRoot({ animation: 150 }),
-    NgxTreeDndModule
+    SortablejsModule.forRoot({animation: 150}),
+    NgxTreeDndModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    NgSelectModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,6 @@
 import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {ActivatedRoute, Router, RouterStateSnapshot, Routes} from '@angular/router';
-import {MediaService} from "./components/_services/media.service";
-import {CategoryService} from "./components/_services/category.service";
-import {LanguageService} from "./components/_services/language.service";
-import {MenuService} from "./components/_services/menu.service";
-import {NewsService} from "./components/_services/news.service";
-import {PageService} from "./components/_services/page.service";
-import {PortfolioService} from "./components/_services/portfolio.service";
-import {SettingService} from "./components/_services/setting.service";
-import {TeamService} from "./components/_services/team.service";
 
 @Component({
   selector: 'app-root',
@@ -18,12 +9,8 @@ import {TeamService} from "./components/_services/team.service";
 })
 export class AppComponent {
   title = 'webiFront';
-  id : string = '5caaf3c2250c062804911d70';
 
-  constructor(private translate: TranslateService, private service : NewsService) {
-    this.service.getOne(this.id).subscribe(data=>{
-      console.log('Data ', data)
-    });
+  constructor(private translate: TranslateService) {
 
     translate.setDefaultLang('en');
     this.translate.currentLang = 'en';
