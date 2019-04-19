d2c6b65a0650085371b5ee7bdd1eb5af6d493dd0
Artur <hayrapet2013@gmail.com>
2019-04-19 16:15:15 +0400

    modules

--- admin/src/app/components/pages/client/client.component.ts
+++ admin/src/app/components/pages/client/client.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ClientService} from '../../_services/client.service';
+import {ClientService} from '../../../_services/client.service';
 
 @Component({
   selector: 'app-client',
