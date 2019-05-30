9b9261202ac382d31032ea9460f2437b86bd3c69
Artur <hayrapet2013@gmail.com>
2019-05-30 09:26:31 +0400

    marge

---
+++ admin/src/app/_services/main.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MainService {
+
+    constructor(public router: Router) {
+    }
+
+    errorHandler(e): void {
+        if (e.statusCode === 401) {
+            this.router.navigate(['login']);
+        }
+    }
+}
--- admin/src/app/components/pages/portfolio/portfolio-list/portfolio-list.component.ts
+++ admin/src/app/components/pages/portfolio/portfolio-list/portfolio-list.component.ts
@@ -5,56 +5,55 @@ import {Router} from '@angular/router';
 import {ItemService} from '../../../../_services/item.service';
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio-list.component.html',
-  styleUrls: ['./portfolio-list.component.css']
+    selector: 'app-portfolio',
+    templateUrl: './portfolio-list.component.html',
+    styleUrls: ['./portfolio-list.component.css']
 })
 export class PortfolioListComponent implements OnInit {
-  portfolios: Portfolio[];
-  delete: any;
-  searchTerm: String;
+    portfolios: Portfolio[];
+    delete: any;
+    searchTerm: String;
 
-  constructor(private dataService: DataService, private router: Router, private itemService: ItemService) {
-  }
-
-  ngOnInit() {
-    this.dataService.getData('portfolio').subscribe(data => {
-      this.portfolios = data['portfolios'];
-    },(err)=>{
-      // console.log(err.error.message);
-      if(err.error.message == 'Auth failed !'){
-        localStorage.clear();
-        // this.router.navigate(['login'])
-      }
-    });
-  }
-
-
-  deletePortfolio(portfolio, i) {
-    this.delete = confirm('Are you want to delete?');
-    if (this.delete == true) {
-      this.dataService.delete('portfolio', portfolio._id).subscribe(data => {
-
-        if (data['success']) {
-          this.portfolios.splice(i, 1);
-        } else {
-          this.router.navigate(['login']);
-        }
-      },(err)=>{
-        console.log(err);
-      });
+    constructor(private dataService: DataService, private router: Router, private itemService: ItemService) {
     }
-  }
 
-  updatePortfolio(portfolio) {
-    this.itemService.portfolio = portfolio;
-    this.router.navigate(['admin/portfolio/edit']);
-  }
+    ngOnInit() {
+        this.dataService.getData('portfolio').subscribe(data => {
+            this.portfolios = data['portfolios'];
+        }, (err) => {
+            if (err.status === 401) {
+                localStorage.clear();
+                this.router.navigate(['login']);
+            }
+        });
+    }
 
 
-  viewPortfolio(portfolio) {
-    this.itemService.portfolio = portfolio;
-  }
+    deletePortfolio(portfolio, i) {
+        this.delete = confirm('Are you want to delete?');
+        if (this.delete == true) {
+            this.dataService.delete('portfolio', portfolio._id).subscribe(data => {
+
+                if (data['success']) {
+                    this.portfolios.splice(i, 1);
+                } else {
+                    this.router.navigate(['login']);
+                }
+            }, (err) => {
+                console.log(err);
+            });
+        }
+    }
+
+    updatePortfolio(portfolio) {
+        this.itemService.portfolio = portfolio;
+        this.router.navigate(['admin/portfolio/edit']);
+    }
+
+
+    viewPortfolio(portfolio) {
+        this.itemService.portfolio = portfolio;
+    }
 
 
 }
